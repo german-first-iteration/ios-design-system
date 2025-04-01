e7b479a513cd5fdee854da5fd0906e341bda6b1f
Ameen Qadri <Ameenk149@gmail.com>
2025-04-01 06:14:31 +0200

    Changed visbility

--- Sources/DesignSystem/Styles/Typography/Typography.swift
+++ Sources/DesignSystem/Styles/Typography/Typography.swift
@@ -9,32 +9,32 @@ import SwiftUI
 
 public extension DesignSystem.Styles.Typography {
   struct Display {
-    static let large = Font.custom("Roboto", size: 57)
-    static let medium = Font.custom("Roboto", size: 45)
-    static let small = Font.custom("Roboto", size: 36)
+    public static let large = Font.custom("Roboto", size: 57)
+    public static let medium = Font.custom("Roboto", size: 45)
+    public static let small = Font.custom("Roboto", size: 36)
   }
   
   struct Headline {
-    static let large = Font.custom("Roboto", size: 32)
-    static let medium = Font.custom("Roboto", size: 28)
-    static let small = Font.custom("Roboto", size: 24)
+    public static let large = Font.custom("Roboto", size: 32)
+    public static let medium = Font.custom("Roboto", size: 28)
+    public static let small = Font.custom("Roboto", size: 24)
   }
   
   struct Title {
-    static let large = Font.custom("Roboto-Regular", size: 22)
-    static let medium = Font.custom("Roboto-Medium", size: 16)
-    static let small = Font.custom("Roboto-Medium", size: 14)
+    public static let large = Font.custom("Roboto-Regular", size: 22)
+    public static let medium = Font.custom("Roboto-Medium", size: 16)
+    public static let small = Font.custom("Roboto-Medium", size: 14)
   }
   
   struct Label {
-    static let large = Font.custom("Roboto-Medium", size: 14)
-    static let medium = Font.custom("Roboto-Medium", size: 12)
-    static let small = Font.custom("Roboto-Medium", size: 11)
+    public static let large = Font.custom("Roboto-Medium", size: 14)
+    public static let medium = Font.custom("Roboto-Medium", size: 12)
+    public static let small = Font.custom("Roboto-Medium", size: 11)
   }
   
   struct Body {
-    static let large = Font.custom("Roboto", size: 16)
-    static let medium = Font.custom("Roboto", size: 14)
-    static let small = Font.custom("Roboto", size: 12)
+    public static let large = Font.custom("Roboto", size: 16)
+    public static let medium = Font.custom("Roboto", size: 14)
+    public static let small = Font.custom("Roboto", size: 12)
   }
 }
