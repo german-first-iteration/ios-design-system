35f03b1b91f0fe9bbf161b324ee829451fa25df1
Ameen Qadri <Ameenk149@gmail.com>
2025-05-29 01:41:46 +0500

    Icon sizes and Type alieases

---
+++ Sources/DesignSystem/Styles/Sizes/IconSizes.swift
@@ -0,0 +1,10 @@
+//
+//  IconSizes.swift
+//  DesignSystem
+//
+//  Created by Muhammad Qadri on 28.05.25.
+//
+
+import SwiftUI
+
+// Generate different variables for icon sizes
---
+++ Sources/DesignSystem/Utilities/Typealiases.swift
@@ -0,0 +1,12 @@
+//
+//  Typealiases.swift
+//  DesignSystem
+//
+//  Created by Muhammad Qadri on 28.05.25.
+//
+
+/// `DSStyle` defines the standard alias for the design system.
+public typealias DSStyle = DesignSystem.Styles
+public typealias DSComponent = DesignSystem.Components
+public typealias DSColor = DesignSystem.Styles.Colors
+
