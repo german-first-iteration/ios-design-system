211175bf4ce67324784f70260c9ccb10059a63af
Ameen Qadri <Ameenk149@gmail.com>
2025-05-30 13:10:21 +0500

    Added DSButton

--- Sources/DesignSystem/Utilities/Typealiases.swift
+++ Sources/DesignSystem/Utilities/Typealiases.swift
@@ -5,6 +5,8 @@
 //  Created by Muhammad Qadri on 28.05.25.
 //
 
+// MARK: STYLE TYPEALIASES
+
 /// `DSStyle` is a shorthand alias for accessing styles within the Design System.
 ///
 /// Example:
@@ -12,14 +14,6 @@
 /// DSStyle.Typography.Title.medium
 /// ```
 public typealias DSStyle = DesignSystem.Styles
-
-/// `DSComponent` is a shorthand alias for accessing reusable UI components from the Design System.
-///
-/// Example:
-/// ```swift
-/// DSComponent.Buttons.FilledPressedButtonStyle(...)
-/// ```
-public typealias DSComponent = DesignSystem.Components
 
 /// `DSColor` is a shorthand alias for accessing standard color definitions from the Design System.
 ///
@@ -46,3 +40,20 @@ public typealias DSIconSize = DesignSystem.Styles.Sizes.Icons
 public typealias DSTypography = DesignSystem.Styles.Typography
 
 
+// MARK: COMPONENTS TYPEALIASES
+
+/// `DSButton` is a shorthand alias for accessing predefined Buttons in the Design System.
+///
+/// Example:
+/// ```swift
+/// DSComponent.Buttons
+/// ```
+public typealias DSButton = DSComponent.Buttons
+
+/// `DSComponent` is a shorthand alias for accessing reusable UI components from the Design System.
+///
+/// Example:
+/// ```swift
+/// DSComponent.Buttons.FilledPressedButtonStyle(...)
+/// ```
+public typealias DSComponent = DesignSystem.Components
